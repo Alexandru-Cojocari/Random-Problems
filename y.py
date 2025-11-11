c756572916015e4376beaa26e9126a1cb53d1510
Alexandru-Cojocari <144117735+Alexandru-Cojocari@users.noreply.github.com>
2025-11-11 14:49:18 +0200

    Update y.py

--- y.py
+++ y.py
@@ -1,3 +1,10 @@
+data_curenta = '2025-11-11'
+def zile_totale(data):
+    parti = data.split('-')
+    an = int(parti[0])
+    luna = int(parti[1])
+    zi = int(parti[2])
+    return an*365 + luna*30 + zi
 Produse = [
     ('Lapte','09.11.2025','23.11.2025',18,n),
     ('Iaurt','23.10.2025','19.11.2025',27,n),
